ec8a68f625e3f008db95bd5833269fab4af0655b
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-06-23 17:08:49 -0300

    feat: add search filter on restaurants page

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -4,23 +4,28 @@ import { IPaginacao } from '../../interfaces/IPaginacao';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
+import { TextField } from '@mui/material';
 
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [proximaPagina, setProximaPagina] = useState('');
+  const [pesquisa, setPesquisa] = useState('');
 
   useEffect(() => {
     // obter restaurantes
     axios.get<IPaginacao<IRestaurante>>('http://192.168.15.57:8000/api/v1/restaurantes/')
       .then(response => {
-        setRestaurantes(response.data.results);
+        setRestaurantes(response.data.results.filter(restaurante => {
+          const regex = new RegExp(pesquisa, 'i');
+          return regex.test(restaurante.nome);
+        }));
         setProximaPagina(response.data.next);
       })
       .catch(erro => {
         console.log(erro);
       });
-  }, []);
+  }, [pesquisa]);
 
   const verMais = () => {
     axios.get<IPaginacao<IRestaurante>>(proximaPagina)
@@ -33,6 +38,13 @@ const ListaRestaurantes = () => {
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    <TextField
+      fullWidth
+      label="Procure o restaurante pelo nome"
+      variant='standard'
+      value={pesquisa}
+      onChange={(e) => setPesquisa(e.target.value)}
+    />
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
     {proximaPagina && <button onClick={verMais}>
       Ver mais

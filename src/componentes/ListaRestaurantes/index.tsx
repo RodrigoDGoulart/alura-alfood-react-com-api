372c23ae9f2e0cd74a151d4551918d02a5e4e1f4
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-06-28 23:01:49 -0300

    feat: order by

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -4,22 +4,34 @@ import { IPaginacao } from '../../interfaces/IPaginacao';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
-import { TextField } from '@mui/material';
+import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material';
 
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [proximaPagina, setProximaPagina] = useState('');
   const [pesquisa, setPesquisa] = useState('');
+  const [ordem, setOrdem] = useState<string>('');
+
+  const ordenar = (parametro: string) => {
+    if (parametro === 'Id') {
+      const restaurantesSort = restaurantes.sort((a, b) => a.id - b.id);
+      setRestaurantes(restaurantesSort);
+    } else if (parametro === 'Nome') {
+      const restaurantesSort = restaurantes.sort((a, b) => a.nome.localeCompare(b.nome));
+      setRestaurantes(restaurantesSort);
+    }
+  }
 
   useEffect(() => {
     // obter restaurantes
     axios.get<IPaginacao<IRestaurante>>('http://192.168.15.57:8000/api/v1/restaurantes/')
       .then(response => {
-        setRestaurantes(response.data.results.filter(restaurante => {
+        const restNome = response.data.results.filter(restaurante => {
           const regex = new RegExp(pesquisa, 'i');
           return regex.test(restaurante.nome);
-        }));
+        });
+        setRestaurantes(restNome);
         setProximaPagina(response.data.next);
       })
       .catch(erro => {
@@ -38,13 +50,38 @@ const ListaRestaurantes = () => {
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
-    <TextField
-      fullWidth
-      label="Procure o restaurante pelo nome"
-      variant='standard'
-      value={pesquisa}
-      onChange={(e) => setPesquisa(e.target.value)}
-    />
+    <div className={style['search-container']}>
+      <TextField
+        fullWidth
+        label="Procure o restaurante pelo nome"
+        variant='standard'
+        value={pesquisa}
+        onChange={(e) => setPesquisa(e.target.value)}
+      />
+      <FormControl variant='standard' sx={{ minWidth: 220 }}>
+        <InputLabel htmlFor='ordem-input'>
+          Ordenar por
+        </InputLabel>
+        <Select
+          inputProps={{
+            id: 'ordem-input'
+          }}
+          value={ordem}
+          onChange={(e: SelectChangeEvent) => {
+            ordenar(e.target.value);
+            setOrdem(e.target.value);
+          }}
+          label="Age"
+        >
+          <MenuItem value={'Id'}>
+            <em>Id</em>
+          </MenuItem>
+          <MenuItem value={'Nome'}>
+            <em>Nome</em>
+          </MenuItem>
+        </Select>
+      </FormControl>
+    </div>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
     {proximaPagina && <button onClick={verMais}>
       Ver mais
